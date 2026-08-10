7e87c7515e7268a7d6e99d7b5dcdc2aad322435f
250130450009gvp-create <250130450009.gvp@gujaratvidyapith.org>
2026-08-10 12:25:29 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,15 @@
+public class Emp{
+    void display (String name){
+        System.out.println("Company Name:"+ name);
+    }
+    void display(String name,int salary,boolean isActive){
+        System.out.println("Employ Name:"+name);
+        System.out.println("Employ Salary:"+salary);
+        System.out.println("Employ Active:"+isActive);
+    }
+  void multi(int a ,int b){
+    int result = a * b;
+    System.out.println("Year Salary:"+result);
+  }
+
+}
---
+++ Empmain.java
@@ -0,0 +1,9 @@
+public class Empmain{
+    public static void main(String[] args){
+  Emp e1 = new Emp();
+  e1.display("TATA");
+  e1.display("brijesh",451000,true);
+ e1.multi(451000,12);
+
+    }
+}
